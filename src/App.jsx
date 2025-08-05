38aa7f545a9f04a9f8195b4f6821b00fb79db455
Bhanu Tripathi <26444713+bhanu75@users.noreply.github.com>
2025-08-05 11:51:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import { AuthProvider } from './context/Hirely';
+import HirelyApp from './context/Hirely';
 
+function App() {
+  return (
+    <AuthProvider>
+      <HirelyApp />
+    </AuthProvider>
+  );
+}
+
+export default App;
